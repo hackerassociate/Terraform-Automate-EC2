30590b583261e479eab79e8bed48082b3469e0de
hackerassociate <62351511+hackerassociate@users.noreply.github.com>
2022-04-16 19:28:51 +0530

    Update Harsh-EC2.tf

--- Harsh-EC2.tf
+++ Harsh-EC2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region = "us-west-1"
-  access_key = "AKIA22QZEBCN6I5KMQVP"
-  secret_key = "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"
-}
-
 resource "aws_instance" "HA-EC2-TEST" {
 
     ami = "ami-009726b835c24a3aa"
